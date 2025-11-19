71d3371f92d59482e5f3a4a09b9358e293497aad
mugoalvin <alvin.mugo@student.moringaschool.com>
2025-11-19 03:05:06 +0300

    Mistake deletion correction

---
+++ components/navigation/native_bottom_tab.tsx
@@ -0,0 +1,33 @@
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { useTheme } from "react-native-paper";
+
+export default function NativeBottomTab() {
+	const theme = useTheme()
+
+	return (
+		<NativeTabs
+			labelVisibilityMode='labeled'
+			backBehavior="history"
+			indicatorColor={theme.colors.secondaryContainer}
+			tintColor={theme.colors.onPrimaryContainer}
+			backgroundColor={theme.dark ? theme.colors.elevation.level1 : theme.colors.elevation.level2}
+		>
+
+			<NativeTabs.Trigger name='(home)'>
+				<Label>Home</Label>
+				<Icon drawable='home' />
+			</NativeTabs.Trigger>
+
+			<NativeTabs.Trigger name='(transactions)'>
+				<Label>Transactions</Label>
+				<Icon src={require("@/assets/icons/bar-chart-2.svg")} />
+			</NativeTabs.Trigger>
+
+			<NativeTabs.Trigger name='settings'>
+				<Label>Settings</Label>
+				<Icon drawable="settings" />
+			</NativeTabs.Trigger>
+
+		</NativeTabs>
+	)
+}
